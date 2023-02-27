ad2f7a38aa89a39a1c464fcc38037e57cb50f1f6
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2023-02-27 17:16:28 +0530

    initial commit

---
+++ HackerRank/src/test/java/com/misc/HackerRank/HackerRankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misc.HackerRank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerRankApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
